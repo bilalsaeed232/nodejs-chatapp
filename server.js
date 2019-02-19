68ad1295921f652a466334c760cf27d85db17850
Bilal Saeed <bilalsaeed232@gmail.com>
2019-02-19 20:58:39 +0100

    ADDED: route for serving messages from server

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//messages object array
+let messages = [{
+        name: 'Tom',
+        message: 'Hi, bilal how are you?'
+    },
+    {
+        name: 'John',
+        message: 'Hi, buddy what\'s going on'
+    }
+];
+
+// define a route to serve those messages to client
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
